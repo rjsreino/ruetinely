f4296dd92865543bdf4735c1d8af8b3c2c276c7b
v60japanesecidecoffee <131238556+v60japanesecidecoffee@users.noreply.github.com>
2025-04-29 23:59:20 +0200

    chore: added two comments

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 <!--trying out a commit I just want to see what it looks like-->
+<!-- second line just because -->
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
