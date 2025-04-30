01ec291d35d801285d6768cda8d71785ee740c72
v60japanesecidecoffee <131238556+v60japanesecidecoffee@users.noreply.github.com>
2025-04-30 13:21:45 +0200

    fix: upgrade Tailwind to v3 and configure properly, add temporary navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  Hello world
-  <RouterView />
+  <div>
+    <h1 class="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+    <NavBar />
+  </div>
 </template>
