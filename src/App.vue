2ecefcd96964f56ee7edbaa5ac60a5a4b8f8ae2d
v60japanesecidecoffee <131238556+v60japanesecidecoffee@users.noreply.github.com>
2025-05-01 14:21:36 +0200

    crashed out making my own calendar restarting from 0

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
 import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <main class="">
-    <header class="bg-blue-200 flex flex-col items-center py-8">
-      <!--div class="bg-green-200 w-2/4 text-center"-->
-      <h1
-        class="text-4xl bg-gradient-to-l from-gray-400 to-stone-900 bg-clip-text text-transparent font-extrabold"
-      >
-        Ruetinely
-      </h1>
-    </header>
-    <section>calendar</section>
-    <section>listview of tasks</section>
+    <header class="bg-blue-200 flex flex-col items-center py-8"></header>
+    <!--div class="bg-green-200 w-2/4 text-center"-->
+    <h1
+      class="text-4xl bg-gradient-to-l from-gray-400 to-stone-900 bg-clip-text text-transparent font-extrabold"
+    >
+      Ruetinely
+    </h1>
+    <section>Calendar</section>
+    <section>Task list</section>
     <NavBar />
   </main>
 </template>
